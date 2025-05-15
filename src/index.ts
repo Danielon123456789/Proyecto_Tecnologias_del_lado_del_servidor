c6b97b7a38752ad82757266a83593f5bf1da7446
PPrez24 <eduardo.perez@iteso.mx>
2025-05-15 00:47:23 -0600

    deploy

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,9 @@
-import app from './app'; // Importa la app desde el nuevo archivo
+import app from './app';
 import connectDB from './config/database';
 import { createServer } from 'http';
-import { Server as SocketIOServer} from 'socket.io';
-import session from 'express-session';
-import passport from 'passport';
-import { swaggerConfig } from './swagger.config';
+import { Server as SocketIOServer } from 'socket.io';
 
 const port = process.env.PORT || 3000;
-
-
-// Servidor HTTP manualmente
 const httpServer = createServer(app);
 
 export const io = new SocketIOServer(httpServer, {
@@ -17,37 +11,10 @@ export const io = new SocketIOServer(httpServer, {
     origin: "*",
     methods: ['GET','POST'],
   },
-})
+});
 
-// Middlewares
-app.use(express.json());
-
-// google auth
-app.use(
-  session({
-    secret: process.env.SESSION_SECRET || 'supersecreto',
-    resave: false,
-    saveUninitialized: false,
-  })
-);
-
-app.use(passport.initialize());
-app.use(passport.session());
-
-app.get('',(req,res)=>{
-    res.send('OK')
-})
-
-// Rutas
-app.use('/', routes);
-
-// Documentación Swagger
-const swaggerDocs = swaggerJsDoc(swaggerConfig);
-app.use('/swagger', serve, setup(swaggerDocs));
-
-// Conexión a la base de datos y arranque
 connectDB().then(() => {
-  app.listen(port, () => {
+  httpServer.listen(port, () => {
     console.log(`Servidor corriendo en el puerto ${port}`);
   });
 }).catch((error) => {
@@ -55,7 +22,6 @@ connectDB().then(() => {
   process.exit(1);
 });
 
-// Manejar eventos de socket.io
 io.on('connection', (socket) => {
   console.log('Nuevo cliente conectado:', socket.id);
 
